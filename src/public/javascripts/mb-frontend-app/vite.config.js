5d91de98495e9f86c07340994a67f67dccbd13c8
tvmota <t.tiago.mota@uello.com.br>
2024-01-13 21:30:06 -0300

    finish configure project

---
+++ src/public/javascripts/mb-frontend-app/vite.config.js
@@ -0,0 +1,31 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/app',
+  
+  build: {
+    outDir: '../../app',
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        assetFileNames: `assets/index.[ext]`,
+        chunkFileNames: `assets/[name].js`,
+        entryFileNames: `assets/index.js`
+      }
+    },
+  },
+  plugins: [
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  
+})
